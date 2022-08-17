bbcbb7c811070e6558af4d0e93dc8d2349ea65b3
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-17 16:13:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Welcome to Flatbook, ";
+let welcomeMessage=currentUser+'!';
+let excitedWelcomeMessage="WELCOME TO FLATBOOK, "+currentUser+'!';
+let shortGreeting="Welcome, "+currentUser[0]+'!';
